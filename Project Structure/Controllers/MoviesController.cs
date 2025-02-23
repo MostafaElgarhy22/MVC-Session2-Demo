2dc364bc1d1b5cbc090047af2234af8175df2afc
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-02-23 08:42:49 +0200

    Action ReturnTypes

--- Project Structure/Controllers/MoviesController.cs
+++ Project Structure/Controllers/MoviesController.cs
@@ -1,39 +1,58 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Project_Structure.Models;
 
 namespace Project_Structure.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly IConfiguration _configuration;
 
-
-
-        //Action: Public Non-Static Object Member Method inside the Controller
-        [HttpGet] // Get: baseUrl/Movies/Index
-        public string Index()
+        public MoviesController(IConfiguration configuration) 
         {
-            return "All Movies";
-        }
+            _configuration = configuration;
+        } 
+
 
         [HttpGet] // Get: baseUrl/Movies/GetMovie/{id}
-        [Authorize]
-        public string GetMovie(int id, string name)
+        public IActionResult GetMovie(int id)
         {
-            return $"Movie with Name : {name} and Id = {id}";
+            if(id == 0)
+           
+              //return new BadRequestResult();
+            return BadRequest();
+
+            if (id == 100)
+
+                // return new NotFoundResult();
+                return NotFound();
+
+            //ContentResult result = new ContentResult();
+            //result.Content = $"<h1> Movie with Id: {id} <h1>";
+
+            //result.ContentType = "text/html";
+            ////result.StatusCode = 200;
+
+            //return result;
+
+            return Content ( $"<h1> Movie with Id: {id} <h1>","text/html");
+            
         }
 
-        [HttpGet] 
-        //[AcceptVerbs("GET", "POST")] 
-        public ViewResult CreateMovie()
-        {
-            return new ViewResult();
-        }
+        //[HttpGet] 
+        //public IActionResult Hamada()
+        //{
+        //    //RedirectResult result = new RedirectResult("https://www.google.com");
+        //    //RedirectToActionResult result = new RedirectToActionResult("GetMovie","Movies", new { id = 10 });
 
-        [HttpPost] 
-        public OkResult CreateMovie(Movie model)
-        {
-            return new OkResult();
-        }
+
+        //    //return Redirect(_configuration["MySettings:RedirectUrl"]?? string.Empty);
+
+        //    //RedirectToRouteResult result = new RedirectToRouteResult("default", new { controller = "Movies", action = "GetMovie", id = 10 });
+        //    //return result;
+        //   // return RedirectToRoute("default",null);
+        //}
+
     }
 }
